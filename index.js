a00fada2db15a131f72745e674f4b61732cd8a91
SmileyDoodle <smileydoodle.official@gmail.com>
2020-06-14 17:11:04 +0300

    - Added Sounds to Button Clicks - Added Animations to User Clicks - Increased difficulty level

--- index.js
+++ index.js
@@ -2,23 +2,55 @@ var buttonColours = ["red", "blue", "yellow", "green"];
 var gamePatten = [];
 var userClickedPattern = [];
 
+//Start the game
+function firstPress() {
+    $("body").one('keypress', function() {
+    nextSequence();
+    });
+  }
+  
+  $(document).ready(function () {
+    firstPress();
+  });
+
+//Random number by computer
 function nextSequence() {
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
+    var name = randomChosenColour;
+
     gamePatten.push(randomChosenColour);
+    
+    playSound(randomChosenColour);
 
+    countLevel(); 
+}
 
-    if ("red" == randomChosenColour) {
+//User button click
+$(".btn").unbind().click(function(event) {
+    var userChosenColour = event.target.id;
+    name = userChosenColour;
+
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+
+    animatePress(event.name);
+  });
+
+//Play-sound function
+function playSound(name) {  
+    if  ("red" == name) {
         $("#red").fadeOut(100).fadeIn(100);
         var red = new Audio('sounds/red.mp3');
         red.play();
     } else if
-        ("blue" == randomChosenColour) {
+        ("blue" == name) {
         $("#blue").fadeOut(100).fadeIn(100);
         var blue = new Audio('sounds/blue.mp3');
         blue.play();
     } else if
-        ("yellow" == randomChosenColour) {
+        ("yellow" == name) {
         $("#yellow").fadeOut(100).fadeIn(100);
         var yellow = new Audio('sounds/yellow.mp3');
         yellow.play();
@@ -27,10 +59,20 @@ function nextSequence() {
         var green = new Audio('sounds/green.mp3');
         green.play();
     }
-
 }
 
-$(".btn").unbind().click(function(event) {
-    var userChosenColour = event.target.id;
-    userClickedPattern.push(userChosenColour);
-  });
+//Add animation when the button is clicked
+function animatePress(currentColour) {
+    var currentColour = name;
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+//Level increased by 1
+function countLevel() {
+    level++;
+    $("#level-title").text("Level " + level);
+}
